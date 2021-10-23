d685d70c21553a7b0846618644d0287ef64ad85a
unknown <slisette127@gmail.com>
2021-10-22 22:16:21 -0500

    added businesses

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
 import React from 'react';
-import { AppBar, Box, Typography, Button } from '@material-ui/core';
 import './App.css';
+import NavBar from './Components/NavBar';
 
-const customTheme = createMuiTheme({
-  palette: {
-    secondary: {
-      main: "#F5BD1F",
-      contrastText: "#6a0dad "
-    }
-  }
-});
+
 
 function App() {
   return (
-<ThemeProvider theme={customTheme}>
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Austin Small Business
-          </Typography>
-          <Button color="inherit">Login</Button>
-      </AppBar>
-    </Box>
-    </ThemeProvider>
-  );
+    <NavBar/>
+  )
 }
 
 export default App;
---
+++ src/Components/Data/SmallBiz.js
@@ -0,0 +1,38 @@
+export default SmallBiz = [
+    {
+        name: 'La Dalat',
+        description: 'Vietnamese Cuisine',
+        address: ' 17 E Avenue B, Temple, TX 76501',
+        operatingHours: '10:30 - 9:00'
+    },
+    {
+        name: 'Thai Cafe',
+        description: 'Thai and Seafood Cuisine',
+        address: '109 W Central Ave, Temple, TX 76501',
+        operatingHours: '11:00 - 8:00'
+    },
+    {
+        name: 'Birdcreek Burger Co.',
+        description: 'Burgers & American comfort food',
+        address: '6 S Main St, Temple, TX 76501',
+        operatingHours: '10:00 - 8:00'
+    },
+    {
+        name: 'Mexiko Cafe',
+        description: 'Mexican and Korean fusion',
+        address: '116 S 1st St Suite A, Temple, TX 76501',
+        operatingHours: '11:00 - 9:00'
+    },
+    {
+        name: '',
+        description: '',
+        address: '',
+        operatingHours: ''
+    },
+    {
+        name: '',
+        description: '',
+        address: '',
+        operatingHours: ''
+    }
+]
